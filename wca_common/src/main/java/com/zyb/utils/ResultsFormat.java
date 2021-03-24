1b50b70e3b3e3a9c0c2c874c7092964d42c80deb
zhaoYongBin <zyb3473966292@163.com>
2021-03-24 08:25:31 +0800

    多盲计算bug

--- wca_common/src/main/java/com/zyb/utils/ResultsFormat.java
+++ wca_common/src/main/java/com/zyb/utils/ResultsFormat.java
@@ -29,7 +29,7 @@ public class ResultsFormat {
 
         int difference = (DbValue - n) / 10000000;//成功个数与99的差值
         int succeed = 99 - difference + lost;//还原个数
-        int sum  = succeed + 2;//挑战个数
+        int sum  = succeed + lost;//挑战个数
 
         return succeed + "/" + sum  + " " + min + ":" + sec;
     }
